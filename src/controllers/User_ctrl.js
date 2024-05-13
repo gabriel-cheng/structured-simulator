84976726bbc7cb446a07dfb5c5b607da4c5c1157
Gabriel Carvalho <gabriel_carvalho.contato@outlook.com>
2024-05-13 13:11:53 -0300

    feat(without verification): finishing the creation of the initial standard queries

--- src/controllers/User_ctrl.js
+++ src/controllers/User_ctrl.js
@@ -1,7 +1,7 @@
 import User from "../models/user_model.js";
 
 class UserCtrl {
-    async viewAll(req, res) {
+    async viewAllUsers(req, res) {
         try {
             const all_users = await User.findAll();
 
@@ -18,11 +18,97 @@ class UserCtrl {
             });
         }
     }
-    createNew(req, res) {
-        return res.json({
-            "response": "Hello, create new!",
-            "status_code": 200
-        });
+    async createNewUser(req, res) {
+        const {
+            user_first_name,
+            user_last_name,
+            user_email,
+            user_phone,
+            user_cpf,
+            user_password
+        } = req.body;
+
+        try {
+            const new_user = await User.create({
+                user_first_name,
+                user_last_name,
+                user_email,
+                user_phone,
+                user_cpf,
+                user_password
+            });
+
+            return res.json({
+                "response": new_user,
+                "status_code": 201
+            });
+        } catch(error) {
+            console.log({error});
+
+            return res.json({
+                "response": {"message": "Internal server error!", error},
+                "status_code": 500
+            });
+        }
+    }
+    async updateUser(req, res) {
+        const { user_id } = req.params;
+
+        const {
+            user_first_name,
+            user_last_name,
+            user_email,
+            user_phone,
+            user_cpf,
+            user_password
+        } = req.body;
+
+        const user_updated = {
+            user_first_name,
+            user_last_name,
+            user_email,
+            user_phone,
+            user_cpf,
+            user_password
+        }
+
+        try {
+            const user = await User.findOne({
+                "where": { user_id }
+            });
+
+            await user.set(user_updated);
+            await user.save();
+
+            return res.json({
+                "response": user,
+                "status_code": 200
+            });
+        } catch(error) {
+            return res.json({
+                "response": {"message": "Internal server error!", error},
+                "status_code": 500
+            });
+        }
+    }
+    async deleteUser(req, res) {
+        const { user_id } = req.params;
+
+        try {
+            await User.destroy({
+                "where": { user_id }
+            });
+
+            return res.json({
+                "response": "User deleted successfuly!",
+                "status_code": 200
+            });
+        } catch(error) {
+            return res.json({
+                "response": {"message": "Internal server error!", error},
+                "status_code": 500
+            });
+        }
     }
 }
 
--- src/router/user_route.js
+++ src/router/user_route.js
@@ -3,7 +3,9 @@ import { Router } from "express";
 const user_ctrl = new UserCtrl();
 const user_route = Router();
 
-user_route.get("/", user_ctrl.viewAll);
-user_route.get("/create", user_ctrl.createNew);
+user_route.get("/", user_ctrl.viewAllUsers);
+user_route.post("/create", user_ctrl.createNewUser);
+user_route.patch("/update/:user_id", user_ctrl.updateUser);
+user_route.delete("/delete/:user_id", user_ctrl.deleteUser);
 
 export default user_route;
