6f779b7afe36cbceefdf7f2496b38774f08cb891
Gabriel Carvalho <gabriel_carvalho.contato@outlook.com>
2024-05-13 13:14:27 -0300

    chore: changing the http update method

--- src/router/user_route.js
+++ src/router/user_route.js
@@ -5,7 +5,7 @@ const user_route = Router();
 
 user_route.get("/", user_ctrl.viewAllUsers);
 user_route.post("/create", user_ctrl.createNewUser);
-user_route.patch("/update/:user_id", user_ctrl.updateUser);
+user_route.put("/update/:user_id", user_ctrl.updateUser);
 user_route.delete("/delete/:user_id", user_ctrl.deleteUser);
 
 export default user_route;
